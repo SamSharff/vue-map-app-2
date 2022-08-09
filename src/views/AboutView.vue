2b08951ba1f65d0017083a643d566bcb88675e0f
Sam Sharff <samanthaleec@gmail.com>
2022-08-09 16:27:32 -0400

    adds misc files

---
+++ src/views/AboutView.vue
@@ -0,0 +1,55 @@
+<script>
+/* global mapboxgl */
+// import FullscreenSlider from "../modules/fullscreen_slider/";
+
+export default {
+  mounted: function () {
+    this.mappy();
+  },
+
+  methods: {
+    mappy: function () {
+      mapboxgl.accessToken = process.env.VUE_APP_MY_API_KEY;
+      const map = new mapboxgl.Map({
+        container: "map", // container ID
+        style: "mapbox://styles/mapbox/streets-v11", // style URL
+        center: [-69.0648, 44.2098], // starting position [lng, lat]
+        zoom: 9, // starting zoom
+        projection: "globe", // display the map as a 3D globe
+      });
+
+      map.on("style.load", () => {
+        map.setFog({}); // Set the default atmosphere style
+      });
+      // create the popup
+      const popup = new mapboxgl.Popup({ offset: 25 }).setText("This is where I live! It is Camden!");
+      const marker1 = new mapboxgl.Marker()
+        .setLngLat([-69.0648, 44.2098])
+        .setPopup(popup) // sets a popup on this marker
+        .addTo(map);
+      console.log(marker1);
+    },
+    // slider: function () {
+    //   const fsSlider = new FullscreenSlider(document, {
+    //     x: window.innerWidth,
+    //     y: window.innerHeight,
+    //   });
+    // },
+  },
+};
+</script>
+<template>
+  <div id="map"></div>
+  <div class="about">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style>
+#map {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+}
+</style>
